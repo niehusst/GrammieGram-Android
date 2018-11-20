2abd7390f0c6bff1a668b29663baf553401f50e1
niehusst <liamniehusstaab@gmail.com>
2018-11-19 21:42:17 -0600

    finished implementing test cases (only base url reset remains)

--- app/src/androidTest/java/com/example/grammiegramco/grammiegram_android/BoardListActivityTest.java
+++ app/src/androidTest/java/com/example/grammiegramco/grammiegram_android/BoardListActivityTest.java
@@ -8,10 +8,12 @@ import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,10 +22,19 @@ import org.mockito.Mockito;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 
+import com.android.dex.util.FileUtils;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
+
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.v4.content.ContextCompat.getSystemService;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
@@ -48,16 +59,27 @@ public class BoardListActivityTest {
 
     // position of an item below the scree fold that must be recycled
     private static final int ITEM_BELOW_FOLD = 20;
+    private MockWebServer server;
 
-    // set up activity to test
+    // set up activity to test (MANUAL LAUNCHING ENABLED)
     @Rule
     public IntentsTestRule<BoardListActivity> rule =
-            new IntentsTestRule<BoardListActivity>(BoardListActivity.class);
+            new IntentsTestRule<BoardListActivity>(BoardListActivity.class, false, false);
 
     @Before
-    public void setUp() {
-        /* TODO: populate the activity recycler with large number of board items (use mockito?)
-           each item has board name (R.string.rv_example_text + position)  */
+    public void setUp() throws IOException {
+        // mock a correct api response from retrofit
+
+        /* some deprecated-ish setup code that may be necessary?
+        //requires extend ImplementationTestCase
+        super.setUp();
+        //mock web server things
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+         */
+
+        server = new MockWebServer();
+        server.start();
+        //TODO: set BASE_URL = server.url("/").toString();
     }
 
     @Before
@@ -67,14 +89,35 @@ public class BoardListActivityTest {
                 .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
 
+    @After
+    public void tearDown() throws IOException {
+        server.shutdown();
+        rule.finishActivity();
+    }
+
     /**
      * Test of all the recycler view functions
      * also test that clicked list item takes to BoardActivity
      */
     @Test
-    public void recyclerTest() {
+    public void recyclerTest() throws UnsupportedEncodingException {
+        // stub api response
+        String fileName = "OkBoardListResponse.json";
+        String jsonContents = new String(FileUtils.readFile(fileName), "UTF8");
+        server.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .setBody(jsonContents));
+
+        // start the activity manually
+        rule.launchActivity(new Intent());
+
         // find recycler view
         ViewInteraction recycler = onView(allOf(withId(R.id.board_recycler), isDisplayed()));
+
+        // check error views are not present
+        onView(withId(R.id.retry_button)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.error_image)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.error_text)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
 
         // scroll to item below the fold
         recycler.perform(actionOnItemAtPosition(ITEM_BELOW_FOLD, scrollTo()));
@@ -97,6 +140,9 @@ public class BoardListActivityTest {
      */
     @Test
     public void settingsButtonTest() {
+        // start the activity manually
+        rule.launchActivity(new Intent());
+
         // find button
         ViewInteraction btn = onView(withId(R.id.settings_button)).check(isDisplayed());
 
@@ -113,6 +159,9 @@ public class BoardListActivityTest {
      */
     @Test
     public void logoutButtonTest() {
+        // start the activity manually
+        rule.launchActivity(new Intent());
+
         // find button
         ViewInteraction btn = onView(withId(R.id.logout_button)).check(isDisplayed());
 
@@ -127,8 +176,17 @@ public class BoardListActivityTest {
      * test that the progress dialogue runs while getting data from server
      */
     @Test
-    public void progressDialogueTest() {
-        // TODO: cause progress dialogue to start
+    public void progressDialogueTest() throws UnsupportedEncodingException {
+        // cause progress dialogue to start: simulate throttled server response
+        String fileName = "OkBoardListResponse.json";
+        String jsonContents = new String(FileUtils.readFile(fileName), "UTF8");
+        server.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .throttleBody(1024, 3, TimeUnit.SECONDS) // lengthen throttling time??
+                .setBody(jsonContents));
+
+        // start the activity manually
+        rule.launchActivity(new Intent());
 
         // check it is displayed
         onView(withId(R.id.progress_dialogue)).check(isDisplayed());
@@ -139,7 +197,6 @@ public class BoardListActivityTest {
         String loadText = getApplicationContext().getResources().   //get str resource from file
                 getString(R.string.loading_text);                    //get text name
         onView(withText(loadText)).check(matches(isDisplayed()));
-
     }
 
     /**
@@ -147,13 +204,17 @@ public class BoardListActivityTest {
      */
     @Test
     public void wifiErrorTest() {
-        // set up mockito spy to check method calls
+        // set up mockito spy to check api calls
         IntentsTestRule<BoardListActivity> boardSpy = Mockito.spy(rule);
 
-        // simulate no wifi
+        // programmatically disable wifi
         Context context = InstrumentationRegistry.getInstrumentation().getContext();
         WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        assert(wifi != null);
         wifi.setWifiEnabled(false);
+
+        // start the activity manually
+        rule.launchActivity(new Intent());
 
         // check image view is displayed
         onView(withId(R.id.error_image)).check(matches(isDisplayed()));
@@ -162,13 +223,14 @@ public class BoardListActivityTest {
         onView(withId(R.id.error_text)).check(isDisplayed());
 
         String errText = getApplicationContext().getResources().   //get str resource from file
-                getString(R.string.wifi_error);                     //get text name
+                getString(R.string.wifi_error);                    //get text name
         onView(withText(errText)).check(matches(isDisplayed()));
 
         // check button is displayed
         ViewInteraction retry = onView(withId(R.id.retry_button)).check(matches(isDisplayed()));
 
         // check clicking button recalls the API
+        retry.perform(click());
         Mockito.verify(boardSpy).getActivity().getBoards();
     }
 
@@ -177,11 +239,19 @@ public class BoardListActivityTest {
      * getting data
      */
     @Test
-    public void serverErrorTest() {
-        // set up mockito spy to check method calls
+    public void serverErrorTest() throws UnsupportedEncodingException {
+        // set up mockito spy to check api calls
         IntentsTestRule<BoardListActivity> boardSpy = Mockito.spy(rule);
 
-        // TODO: simulate server err
+        // simulate server err with stub
+        String fileName = "ServerErrorResponse.json";
+        String jsonContents = new String(FileUtils.readFile(fileName), "UTF8");
+        server.enqueue(new MockResponse()
+                .setResponseCode(500)
+                .setBody(jsonContents));
+
+        // start the activity manually
+        rule.launchActivity(new Intent());
 
         // check image view is displayed
         onView(withId(R.id.error_image)).check(matches(isDisplayed()));
@@ -197,7 +267,7 @@ public class BoardListActivityTest {
         ViewInteraction retry = onView(withId(R.id.retry_button)).check(matches(isDisplayed()));
 
         // check clicking button recalls the API
+        retry.perform(click());
         Mockito.verify(boardSpy).getActivity().getBoards();
-
     }
 }
